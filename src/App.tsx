48635160c86aeec8d62c753e25f31d379684ff98
markoka101 <markoka101@gmail.com>
2025-10-20 22:42:34 -0400

    registered chart components and added the navbar and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { useState } from 'react';
+import { Home } from './pages/Home';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	ArcElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
+} from 'chart.js';
+import { Navbar } from './components/navbar';
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	ArcElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler
+);
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <main></main>;
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	return (
+		<main className="flex min-h-[100vh] overflow-x-hidden overflow-y-auto">
+			<Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+			<Home />
+		</main>
+	);
 }
 
 export default App;
